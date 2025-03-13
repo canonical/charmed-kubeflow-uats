dcf8d64c034fa35c1fec13280f26947c354484b0
Michal Hucko <michal.hucko@canonical.com>
2025-03-13 18:21:28 +0100

    Update test dependencies for 1.10 release (#165)
    Update SDKs and dependencies for 1.10 release.
    
    Closes: canonical/kfp-operators#594
    Closes: https://warthogs.atlassian.net/browse/KF-7056

--- driver/test_kubeflow_workloads.py
+++ driver/test_kubeflow_workloads.py
@@ -37,7 +37,7 @@ PROFILE_TEMPLATE_FILE = ASSETS_DIR / "test-profile.yaml.j2"
 TESTS_LOCAL_RUN = eval(os.environ.get("LOCAL"))
 TESTS_LOCAL_DIR = os.path.abspath(Path("tests"))
 
-TESTS_IMAGE = "kubeflownotebookswg/jupyter-scipy:v1.9.0"
+TESTS_IMAGE = "kubeflownotebookswg/jupyter-scipy:v1.10.0-rc.1"
 
 NAMESPACE = "test-kubeflow"
 PROFILE_RESOURCE = create_global_resource(
