e584173859aaaca72c509f8205c6d0fbff03b1b4
Dario Faccin <35623244+dariofaccin@users.noreply.github.com>
2025-11-21 14:00:19 +0100

    feat: add UATs for Kubeflow Trainer V2 (#216) (#224)
    (cherry picked from commit 41406d49164b37ee4b06bb65cf76c7b498b3cd27)
    
    Signed-off-by: Dario Faccin <dario.faccin@canonical.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,6 +10,8 @@ def pytest_addoption(parser: Parser):
     """Add pytest options.
     * Add an `--include-gpu-tests` flag to include the tests under the `gpu` directory
       in the executed tests.
+    * Add an `--include-kubeflow-trainer-tests` flag to include the tests for Kubeflow Trainer V2
+      in the executed tests.
     """
     parser.addoption(
         "--include-gpu-tests",
@@ -17,7 +19,16 @@ def pytest_addoption(parser: Parser):
         help="Defines whether to include tests under the `gpu` directory in the executed tests."
         "By default, it is set to False.",
     )
+    parser.addoption(
+        "--include-kubeflow-trainer-tests",
+        action="store_true",
+        help="Defines whether to include the tests for Kubeflow Trainer V2 in the executed tests."
+        "By default, it is set to False.",
+    )
 
 
 def pytest_configure(config):
     os.environ["include_gpu_tests"] = str(config.getoption("--include-gpu-tests"))
+    os.environ["include_kubeflow_trainer_tests"] = str(
+        config.getoption("--include-kubeflow-trainer-tests")
+    )
